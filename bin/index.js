a17ca71e4c50ba93b14ce5ea3bea6660ccb5d21c
lingziyb <zhaolinglizll@qq.com>
2019-11-05 12:30:15 +0800

    finish

---
+++ bin/index.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const https = require("https");
+const http = require("http");
+const path = require("path");
+
+const regex = /\!\[.*\]\((http.*)\)/;
+const postDirPath = path.resolve(__dirname, "./../example");
+
+function main() {
+  const files = fs.readdirSync(postDirPath, {
+    withFileTypes: true
+  });
+
+  files.forEach(file => {
+    if (file.isFile()) replaceFile(file);
+  });
+}
+
+async function replaceFile(file, imgIndex = 0) {
+  imgIndex++;
+  const filePath = path.resolve(postDirPath, file.name);
+  const fileData = fs.readFileSync(filePath, "utf8");
+
+  if (!regex.exec(fileData)) return;
+  const url = regex.exec(fileData)[1];
+
+  const imgData = await download(url);
+  const dirName = path.basename(file.name, ".md");
+  saveImg(dirName, imgIndex, imgData);
+  replace(filePath, imgIndex, url);
+
+  replaceFile(file, imgIndex);
+}
+
+function download(url) {
+  return new Promise((resolve, reject) => {
+    const HTTP = url.includes("http://") ? http : https;
+    HTTP.get(url, response => {
+      let imgData = "";
+      response.setEncoding("binary");
+      if (response.statusCode == 301) download(response.headers.location);
+
+      response.on("data", chunk => (imgData += chunk));
+      response.on("end", () => {
+        resolve(imgData);
+      });
+    }).on("error", err => reject(err));
+  });
+}
+
+function saveImg(dirName, imgFileName, imgData) {
+  const dirPath = path.resolve(postDirPath, dirName);
+  if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath);
+  fs.writeFileSync(`${dirPath}/${imgFileName}.png`, imgData, "binary");
+}
+
+function replace(filePath, imgFileName, url) {
+  const fileData = fs.readFileSync(filePath, "utf8");
+  const newFile = fileData.replace(url, `./${imgFileName}.png`);
+  fs.writeFileSync(filePath, newFile);
+}
+
+main();
---
+++ readme.md
@@ -0,0 +1,14 @@
+### what it is
+把md文档中的图片地址换成相对地址。
+
+### run
+
+```
+npm run dev
+```
+
+就可以把example例文里的图片，下载并换成相对地址了。
+
+##### 详细文档参考
+[把md文档中的图片地址换成相对地址](https://www.jianshu.com/p/440a0cc48db0)
+
